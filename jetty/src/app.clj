2028fdc4044b05f8d68c08a7b951184cae497c7a
fallenwood <fallenwood.y@outlook.com>
2025-01-13 23:02:05 +0800

    Update jetty to 12.0

--- jetty/src/app.clj
+++ jetty/src/app.clj
@@ -1,24 +1,34 @@
 (ns app
   (:import
-    (org.eclipse.jetty.server Server)
-    (org.eclipse.jetty.server.handler AbstractHandler)))
+   (org.eclipse.jetty.server Server)
+   (org.eclipse.jetty.server Handler)
+   (org.eclipse.jetty.server ServerConnector)
+   (org.eclipse.jetty.server Handler$Abstract)
+   (org.eclipse.jetty.server Handler$Abstract$NonBlocking)
+   (org.eclipse.jetty.io Content$Sink)
+   (org.eclipse.jetty.http HttpHeader)))
 
 (defn make-default-handler []
-  (proxy [AbstractHandler] []
-    (handle [target base-request request response]
-      (.setContentType response "text/html;charset=utf-8")
-      (.setHandled base-request true)
-      (.setStatus response 200)
-      (.println (.getWriter response) "Hello World"))))
+  (proxy [Handler$Abstract$NonBlocking] []
+    (handle
+     [request response callback]
+     (doto (.getHeaders response)
+       (.put HttpHeader/SERVER "text/html;charset=utf-8")
+       (.put "Server" "Clojure/Jetty 12.0"))
+     (.setStatus response 200)
+     (Content$Sink/write response true "Hello World" callback)
+     true)))
 
 (defn make-server [port]
-  (let [server (new Server 8080)]
-    (.setHandler
-      server
-      (make-default-handler))
-    server))
+  (let [server (Server.)
+        connector (ServerConnector. server)]
+      (.setPort connector port)
+      (.addConnector server connector)
+      (.setHandler
+       server
+       (make-default-handler))
+      server))
 
 (defn main [opts]
   (let [server (make-server 8080)]
-    (.start server)
-    (.join server)))
+    (.start server)))
